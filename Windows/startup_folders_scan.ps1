c9c1d438d57e1637a3956a2298ee0205d403ce35
Oscar <148647012+oscarxmt@users.noreply.github.com>
2026-09-16 13:43:38 +0200

    Updated Powershell Scanners

--- Windows/startup_folders_scan.ps1
+++ Windows/startup_folders_scan.ps1
@@ -7,17 +7,32 @@ $user = $env:username
 $finalfilename = $user + "_ScheduledTask"
 
 Write-Host "`n[*] Checking Startup Folders..."
-Get-ChildItem "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-Get-ChildItem "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+
+
+if(test-path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup")
+{
+  $Current_User_Startup = Get-ChildItem "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+}
+else
+{
+  write-host "[!] Current user startup folder not found."
+}
+if(test-path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup")
+{
+   $All_Users_Startup = Get-ChildItem "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+}
+else
+{
+  write-host "[!] All users startup folder not found."
+}
+write-host "[*] Successfully scanned startup folders for current user and all users."
 
 # This not worky yet :(
-#[Json]$MyJsonVariable = "
-#{
-#    ""Startup_Folders"": {
-#        ""User_Startup_Folder"": ""$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"",
-#        ""All_Users_Startup_Folder"": ""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup""
-#    }
-#
-#
-#}
-#"
+[Json]$MyJsonVariable = "
+{
+    "type": "Current User Startup Folder",
+    "location": "$Current_User_Startup",
+
+
+
+}
