315f298b907ad0420c143296f53919a43f004965
Oscar <148647012+oscarxmt@users.noreply.github.com>
2026-09-16 07:53:55 +0200

    Deleted Win_main.ps1
    To clear out

--- Windows/win_main.ps1
+++
@@ -1,7 +0,0 @@
-# Main script for windows persistence scanning
-
-
-
-Write-host "[!] Starting Windows persistence scanning..."
-
-# add json support in the scripts being ran and make a folder for them.
